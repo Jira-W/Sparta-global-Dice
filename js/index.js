60c17a09a7604f562a8ce196f69dc26fedcb1cb4
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-04-25 22:29:29 +0100

    Homework submission

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code here
+var num =0;
+function diceroll(){
+  clearscreen()
+   num = Math.floor((Math.random()*6)+1);
+
+   console.log(num);
+  if (num==1){
+    document.getElementById('mid').classList.add("dot")
+  }
+  else if (num==2){
+    document.getElementById('mid-left').classList.add("dot")
+    document.getElementById('mid-right').classList.add("dot")
+  }
+  else if (num==3){
+    document.getElementById('top-left').classList.add("dot")
+    document.getElementById('mid').classList.add("dot")
+    document.getElementById('bot-right').classList.add("dot")
+  }
+  else if (num==4){
+    document.getElementById('bot-left').classList.add("dot")
+    document.getElementById('top-left').classList.add("dot")
+    document.getElementById('top-right').classList.add("dot")
+    document.getElementById('bot-right').classList.add("dot")
+  }
+  else if (num==5){
+    document.getElementById('mid').classList.add("dot")
+    document.getElementById('bot-left').classList.add("dot")
+    document.getElementById('top-left').classList.add("dot")
+    document.getElementById('top-right').classList.add("dot")
+    document.getElementById('bot-right').classList.add("dot")
+  }
+  else if (num==6){
+    document.getElementById('top-right').classList.add("dot")
+    document.getElementById('mid-left').classList.add("dot")
+    document.getElementById('mid-right').classList.add("dot")
+    document.getElementById('bot-right').classList.add("dot")
+    document.getElementById('bot-left').classList.add("dot")
+    document.getElementById('top-left').classList.add("dot")
+  }
+}
+function clearscreen(){
+  document.getElementById('mid').classList.remove("dot")
+  document.getElementById('mid-left').classList.remove("dot")
+  document.getElementById('mid-right').classList.remove("dot")
+  document.getElementById('top-left').classList.remove("dot")
+  document.getElementById('top-right').classList.remove("dot")
+  document.getElementById('top-mid').classList.remove("dot")
+  document.getElementById('bot-left').classList.remove("dot")
+  document.getElementById('bot-right').classList.remove("dot")
+  document.getElementById('bot-mid').classList.remove("dot")
+}
